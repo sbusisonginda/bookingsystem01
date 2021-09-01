ae227443080e76430aad6a81de74614537ffa33c
Sibusiso <62797158@mylife.unisa.ac.za>
2021-09-01 21:04:24 +0200

    new changes

--- index.php
+++ index.php
@@ -19,18 +19,18 @@ if ($text == "") {
     $response .= "1. Collection";
     $response .= "2. Re-Application";
   } else if ($text == "1*1") {
-    $collectionDate = ". date("Y-m-d")";
+    $collectionDate = "2021-11-04";
     $response = "END your birth certificate is ready for collection on ".$collectionDate;
 
   } else if ($text == "1*2") {
     $newApplicationDate = "2021-11-04"; 
-    $response = "END for new application you can come on this date " $newApplicationDate
+    $response = "END for new application you can come on this date ".$newApplicationDate;
   } else if ($text == "2*1") {
-    $collectionDate = ". date("Y-m-d")";
+    $collectionDate = "2021-11-04";
     $response = "END your ID is ready for collection on ".$collectionDate;
   } else if ($text == "2*2") {
     $newApplicationDate = "2021-11-04"; 
-    $response = "END for new application you can come on this date " $newApplicationDate
+    $response = "END for new application you can come on this date ".$newApplicationDate;
   }
 
   header('Content-type: text/plain');
